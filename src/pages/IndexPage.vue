ef92a0864baacaefe1771e74774b0d841099431c
ali <aliesmaeeli20@gmail.com>
2022-08-03 23:23:06 +0430

    new quasar

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page>
+    <q-card class="q-ma-md">
+      <q-card-section>
+        <q-file v-model="excelFile" @change="onFileChange" />
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  data () {
+    return {
+      excelFile: null
+    }
+  },
+  methods: {
+    onFileChange (data) {
+      console.log('data', data)
+    }
+  }
+})
+</script>
