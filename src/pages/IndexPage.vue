56fc75086894b8cd0d4f23a7ffa6ae2246bce6f5
ali <aliesmaeeli20@gmail.com>
2022-08-04 00:28:34 +0430

    read bank excel

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,6 +3,12 @@
     <q-card class="q-ma-md">
       <q-card-section>
         <q-file v-model="excelFile" @update:model-value="onFileChange" />
+        <q-table
+          title="بانک"
+          :rows="mainBank"
+          :columns="tableColumns"
+          row-key="کد ملی"
+        />
       </q-card-section>
     </q-card>
   </q-page>
@@ -16,26 +22,51 @@ export default defineComponent({
   name: 'IndexPage',
   data () {
     return {
-      excelFile: null
+      excelFile: null,
+      tableColumns: [],
+      mainBank: []
     }
   },
   methods: {
+    getMainBankFromExcel (data) {
+      data.splice(0, 1)
+      // const newData = data.filter(row => Object.keys(row).length === 40)
+
+      return data
+    },
+    setTableColumns (firstRow) {
+      Object.keys(firstRow).forEach(columnKey => {
+        this.tableColumns.push({
+          name: columnKey,
+          label: columnKey,
+          align: 'left',
+          field: row => row[columnKey],
+          sortable: true
+        })
+      })
+    },
     onFileChange (file) {
       console.log('file: ', file)
 
       const reader = new FileReader()
 
-      reader.onload = function (e) {
+      reader.onload = (e) => {
         const data = e.target.result
         const workbook = XLSX.read(data, {
           type: 'binary'
         })
 
-        workbook.SheetNames.forEach(function (sheetName) {
+        workbook.SheetNames.forEach((sheetName) => {
           // Here is your object
           const xlRowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName])
           // const jsonObject = JSON.stringify(xlRowObject)
-          console.log('xlRowObject', xlRowObject)
+          if (sheetName.trim() === 'بانک') {
+            this.mainBank = this.getMainBankFromExcel(xlRowObject)
+            this.setTableColumns(this.mainBank[0])
+            console.log('this.mainBank: ', this.mainBank)
+          }
+          // console.log('sheetName: ', sheetName)
+          // console.log('xlRowObject: ', xlRowObject)
           // console.log('jsonObject', jsonObject)
         })
       }
